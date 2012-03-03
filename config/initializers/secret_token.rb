e34e12183689aa1f5302d7173474a938f3b88f3c
cw42143977 <craig.wilmington@students.mq.edu.au>
2012-03-03 17:44:45 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssOne::Application.config.secret_token = '3ee0673676b52da5594179d58fc806b3b04a1566cb427ca35c1a4e1cef51e9b9da09493abb4db645e6a4a1ae94f6ef59d1d5dc3c3127021ea51f8866bdaffea0'
